f8d2086ef6a564343de97951c43d2b4407deb952
Todd Whitelow <todd@whitelow.tech>
2023-03-13 02:38:15 -0600

    add services for all movies

--- src/main/java/dev/todd/MoviesAPI/Movie.java
+++ src/main/java/dev/todd/MoviesAPI/Movie.java
@@ -3,9 +3,11 @@ package dev.todd.MoviesAPI;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import java.util.List;
 
 @Document(collection = "movies")
 @Data
