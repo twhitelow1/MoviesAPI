fc030ce5ed4a7a2e6b5d66d90c6e7da011dd1696
Todd Whitelow <todd@whitelow.tech>
2023-03-13 03:29:17 -0600

    add revies service and repository

---
+++ src/main/java/dev/todd/MoviesAPI/ReviewController.java
@@ -0,0 +1,10 @@
+package dev.todd.MoviesAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class ReviewController {
+    
+}
--- src/main/java/dev/todd/MoviesAPI/ReviewService.java
+++ src/main/java/dev/todd/MoviesAPI/ReviewService.java
@@ -1,10 +1,25 @@
 package dev.todd.MoviesAPI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId) {
+        Review review = new reviewRepository.insert(new Review(reviewBody));
 
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("ImdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
