68920194b4d98fe610f25be69a1130c9dd2720b1
Todd Whitelow <todd@whitelow.tech>
2023-03-13 02:55:35 -0600

    add single movie endpoints

--- src/main/java/dev/todd/MoviesAPI/MoviesApiApplication.java
+++ src/main/java/dev/todd/MoviesAPI/MoviesApiApplication.java
@@ -2,8 +2,6 @@ package dev.todd.MoviesAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.processing.Generated;
 
